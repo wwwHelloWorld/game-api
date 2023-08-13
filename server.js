0f27258ef3c30ab41528b37255559ba4b80fc1bd
Hello World <83601958+wwwHelloWorld@users.noreply.github.com>
2023-08-13 20:29:07 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+import express, { json } from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(json()); // Middleware для парсинга JSON-данных
+app.use(cors()); // Enable CORS for all routes
+
+// Пример начального массива данных
+// eslint-disable-next-line max-len
+let data = {
+  gameData: [
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+  ],
+  isActiveGame: true,
+  activeSing: null,
+  isReset: false,
+  isGameOver: false,
+};
+
+// const gameOver = () => {
+//   return data.gameData.every((el) => el !== 'empty');
+// };
+
+// Роут для получения текущего состояния массива
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+
+// Роут для обновления массива
+app.post('/update', (req, res) => {
+  const newData = req.body; // Получение новых данных из тела запроса
+  data = newData; // Обновление массива данных
+  console.log(data);
+  res.json(newData);
+});
+
+app.listen(3005, () => {
+  console.log('Сервер запущен на порту 3005');
+});
